37a8e128842de35e9fa2cda8b6aeb5a3c72097c5
Gregor von Laszewski <laszewski@gmail.com>
2014-05-08 06:14:15 -0400

    fixing the timer template so it does not crash

--- cmd3/plugins/timer.py
+++ cmd3/plugins/timer.py
@@ -14,21 +14,30 @@ class timer:
     def do_timer(self, args, arguments):
         """
         Usage:
-            timer (on|off)
+            timer on
+            timer off            
+            timer list 
 
-        switches timers on and off not yet implemented
+        Description:
+        
+             switches timers on and off not yet implemented.
+             If the timer is on each command will be timed and its
+             time is printed after the command. Please note that
+             background command times are not added.
         """
+        print arguments
+        print "args", args
         args = args.lower()
 
         if args in ("on", "off"):
-            self.with_timers = (line == "on")
+            self.with_timers = (args == "on")
             print "Timers are now:", self.with_timers
         if args == 'list':
             self.list_timers()
         else:
-            self.help_timer()
+            self.do_timer.__doc__
 
-    def list_tmers(self):
+    def list_timers(self):
         """list of all timers"""
         print "TODO"
         pass
