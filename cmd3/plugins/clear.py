a27e2c09e9280f28a1dec55c03853fb9ad786806
Gregor von Laszewski <laszewski@gmail.com>
2014-05-08 05:56:09 -0400

    adding more options to the banner

--- cmd3/plugins/clear.py
+++ cmd3/plugins/clear.py
@@ -28,15 +28,27 @@ class clear:
     def do_banner(self, arg, arguments):
         """
         Usage:
-            banner [--c=CHAR] TEXT
+            banner [-c CHAR] [-n WIDTH] [-i INDENT] TEXT
 
         Arguments:
             TEXT   The text message from which to create the banner
-            CHAR   The character for the frame. [default='=']
+            CHAR   The character for the frame. 
+            WIDTH  Width of the banner
+            INDENT indentation of the banner
             
-        Prints a banner form a one line text message."""
+        Options:
+            -c CHAR   The character for the frame. [default: #]
+            -n WIDTH  The width of the banner. [default: 70]
+            -i INDENT  The width of the banner. [default: 0]            
+            
+        Prints a banner form a one line text message.
+        """
 
-        print 70 * arguments['CHAR']
-        print arguments['TEXT']
-        print 70 * arguments['CHAR']         
+        n = int(arguments['-n'])
+        c = arguments['-c']
+        i = int(arguments['-i'])        
+        
+        print i * " " + (n-i) * c
+        print i * " " + c, arguments['TEXT']
+        print i * " " + (n-i) * c
 
