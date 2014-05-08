1b8f56e42b9e78a5d17800ed0fb81f8431c39115
Gregor von Laszewski <laszewski@gmail.com>
2014-05-08 05:37:19 -0400

    added a banner

--- cmd3/plugins/clear.py
+++ cmd3/plugins/clear.py
@@ -23,3 +23,20 @@ class clear:
         Clears the screen."""
 
         sys.stdout.write(os.popen('clear').read())
+
+    @command
+    def do_banner(self, arg, arguments):
+        """
+        Usage:
+            banner [--c=CHAR] TEXT
+
+        Arguments:
+            TEXT   The text message from which to create the banner
+            CHAR   The character for the frame. [default='=']
+            
+        Prints a banner form a one line text message."""
+
+        print 70 * arguments['CHAR']
+        print arguments['TEXT']
+        print 70 * arguments['CHAR']         
+
