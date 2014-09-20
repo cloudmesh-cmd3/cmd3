77141c8cac2ee1a1fd71de04d6654e8fa7189fd5
Gregor von Laszewski <laszewski@gmail.com>
2014-09-20 10:00:34 -0400

    adding a console theme

--- cmd3/console.py
+++ cmd3/console.py
@@ -1,19 +1,27 @@
 import textwrap
 
-HEADER = '\033[95m'
-OKBLUE = '\033[94m'
-OKGREEN = '\033[92m'
-#WARNING = '\033[93m' # yellow
-FAIL = '\033[91m'
-WARNING = FAIL
-ENDC = '\033[0m'
-BOLD = "\033[1m"
-
-class Console(object):
     
-    def __init__(self, _color_on = True):
-        self._color_on = _color_on
+class Console(object):
 
+    def __init__(self, _color_on = True):
+        self._color_on = True
+        self.theme = {
+            'HEADER': '\033[95m',
+            'OKBLUE': '\033[34m',
+            'OKGREEN': '\033[32m',
+            'FAIL': '\033[31m',
+            'WARNING': '\033[31m',
+            'ENDC': '\033[m',
+            'BOLD': "\033[1m",
+        }
+
+
+    def theme(self, theme):
+        self.theme = theme
+        
+    def color(self, on):
+        self._color_on = on
+                
     def _msg(self, message, width=90):
         return textwrap.fill(message, width=width)
 
@@ -22,36 +30,39 @@ class Console(object):
 
     def error(self, message):
         if self._color_on:
-            print FAIL + self._msg("ERROR: " + message) + ENDC
+            print self.theme['FAIL'] + self._msg("ERROR: " + message) + self.theme['ENDC']
         else:
             print self._msg("ERROR: " + message)
 
     def info(self, message):
         if self._color_on:
-            print OKBLUE + self._msg("INFO: " + message) + ENDC
+            print self.theme['OKBLUE'] + self._msg("INFO: " + message) + self.theme['ENDC']
         else:
             print self._msg("INFO: " + message)
 
     def warning(self, message):
         if self._color_on:
-            print WARNING + self._msg("WARNING: " + message) + ENDC
+            print self.theme['WARNING'] + self._msg("WARNING: " + message) + self.theme['ENDC']
         else:
             print self._msg("WARNING: " + message)
 
     def ok(self, message):
         if self._color_on:
-            print OKGREEN + self._msg(message) + ENDC
+            print self.theme['OKGREEN'] + self._msg(message) + self.theme['ENDC']
         else:
             print self._msg(message)
 
 #
 # Example
 #
-# from cmd3.console import Console
 
-# console = Console()
+"""
+from cmd3.console import Console
 
-# console.warning("Warning")
-# console.error("Error")
-# console.info("Info")
-# console.msg("msg")
+console = Console()
+
+console.warning("Warning")
+console.error("Error")
+console.info("Info")
+console.msg("msg")
+"""
