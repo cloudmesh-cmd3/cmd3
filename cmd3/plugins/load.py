7dbdff1da51aa8f452c3664de62d3ae3f7b0b3ed
Gregor von Laszewski <laszewski@gmail.com>
2014-10-30 22:08:10 -0400

    non working load

---
+++ cmd3/plugins/load.py
@@ -0,0 +1,30 @@
+from cmd3.shell import command
+
+
+class load:
+
+    def activate_load(self):
+        """activates the load command"""
+        pass
+
+    @command
+    def do_load(self, arg, arguments):
+        """
+        ::
+
+            Usage:
+                load MODULE
+
+            Loads the plugin given a specific module name. The plugin must be ina plugin directory.
+
+            Arguments:
+               MODULE  The name of the module.
+        """
+        module = arguments["MODULE"]
+        # try:
+        #    plugins.append(dict(get_plugins_from_module(module_name)))
+        # except:
+        #    #print "WARNING: could not find", module_name
+        #    pass
+        print module
+
