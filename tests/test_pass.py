195260ce337c9ba0fdb05684d18aaa5c4789e4ed
Gregor von Laszewski <laszewski@gmail.com>
2015-03-30 10:52:45 -0400

    remove imports from test

--- tests/test_pass.py
+++ tests/test_pass.py
@@ -8,20 +8,6 @@ nosetests -v
 
 """
 
-from cloudmesh.config.cm_config import cm_config_server
-from cloudmesh.config.cm_config import cm_config
-from cloudmesh_base.ConfigDict import ConfigDict
-
-import json
-import os
-import warnings
-from pprint import pprint
-
-from cloudmesh_base.util import HEADING
-from cloudmesh_base.util import path_expand
-from cloudmesh_base.locations import config_file
-
-
 class Test_pass:
 
     def setup(self):
